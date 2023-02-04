47808c410ca0519ba309ff63fbbc19a860e8b8b9
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2023-02-04 14:55:34 +0700

    Scanf

--- main.go
+++ main.go
@@ -4,56 +4,31 @@ import (
 	"fmt"
 )
 
-// var x int64 = 1_234
-
-// func(int,int)int
-func sum(x int, y int) int {
-	return x + y
-}
-
 func main() {
-	// main function
 
-	i := sum(10, 20)
-	fmt.Printf("%T %#v\n", i, i)
-
-	// s := "Golang"
-	// i := 1_234
-	// f := 1_234.567
-	// fmt.Printf("%T %#v\n", s, s)
-	// fmt.Printf("%T %#v\n", i, i)
-	// fmt.Printf("%T %#v %.2f\n", f, f, f)
-
-	// fmt.Printf("%T %#v\n", x, x)
-
-	// str1 := fmt.Sprint(math.MaxInt)
-	// if i2, err := strconv.ParseInt(str1, 10, 64); err != nil {
-	// 	fmt.Println("Error converting string to number")
-	// } else {
-	// 	fmt.Printf("%T %#v\n", i2, i2)
-
-	// int_array := [...]int{1, 2, 3}
-	// int_array[0] = 0
-	// fmt.Printf("%T %#v\n", int_array, int_array)
-
-	// int_slice := []int{1, 2, 3}
-	// int_slice[0] = 0
-	// int_slice = append(int_slice, 4)
-	// fmt.Printf("%T %#v\n", int_slice, int_slice)
-	// fmt.Printf("%T %#v\n", int_slice[:1], int_slice[:1])
-
-	// countries := map[string]string{}
-	// countries["th"] = "Thailand"
-	// countries["en"] = "United Kingdom"
-	// if country, ok := countries["en"]; ok {
-	// 	fmt.Printf("%T %#v\n", country, country)
-	// } else {
-	// 	fmt.Println("No value")
+	// type X struct {
+	// 	name string
+	// 	next *X
 	// }
+	// x := X{name: "one",
+	// 	next: &X{name: "two",
+	// 		next: &X{name: "three", next: nil}}}
+	// fmt.Println(x.name)
+	// fmt.Println(x.next.name)
+	// fmt.Println(x.next.next.name)
+	// fmt.Println(x.next.next.next)
 
-	// values := []int{10, 20, 30, 40, 50, 60}
-	// for i, v := range values {
-	// 	fmt.Println(i, v)
+	// type Employee struct {
+	// 	name string
 	// }
+	// employees := []Employee{}
+	// employees = append(employees, Employee{name: "Nattapon"})
+	// fmt.Printf("%T %#v\n", employees, employees)
+
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("%T %#v\n", name, name)
+	fmt.Printf("%T %#v\n", &name, &name)
 
 }
