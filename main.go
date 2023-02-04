c5900137c8cd80977951e30716d709ac480fc72b
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2023-02-04 15:34:42 +0700

    math.Pi

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
@@ -23,16 +26,29 @@ func main() {
 	// employees = append(employees, Employee{name: "Nattapon"})
 	// fmt.Printf("Var type: %T - value: %#v\n", employees, employees)
 
-	var name string
-	fmt.Print("Enter your name: ")
-	_, err := fmt.Scanf("%s", &name)
-	if err != nil {
-		fmt.Printf("%T - %#v\n", err, err)
-	} else {
-		fmt.Printf("%T - %#v\n", name, name)
-		if name == "Nattapon" {
-			fmt.Printf("Hello %s\n", name)
-		}
-	}
+	// var name string
+	// fmt.Print("Enter your name: ")
+	// _, err := fmt.Scanf("%s", &name)
+	// if err != nil {
+	// 	fmt.Printf("%T - %#v\n", err, err)
+	// } else {
+	// 	fmt.Printf("%T - %#v\n", name, name)
+	// 	if name == "Nattapon" {
+	// 		fmt.Printf("Hello %s\n", name)
+	// 	}
+	// }
+
+	// i := 1234
+	// s := strconv.Itoa(i)
+	// fmt.Printf("Var type %T - value %#v\n", s, s)
+	// s2 := "ABCD"
+	// var err error
+	// i, err = strconv.Atoi(s2)
+	// fmt.Printf("Var type %T - value %#v\n", s2, s2)
+	// fmt.Printf("Var type %T - value %#v\n", err, err)
+	// fmt.Printf("Var type %T - value %#v\n", i, i)
+
+	const pi = math.Pi
+	fmt.Printf("Var type %T - value %#v\n", pi, pi)
 
 }
