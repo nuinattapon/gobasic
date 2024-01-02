952e94aba89e6df41390ffef73426b89fee018e9
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2024-01-02 15:23:09 +0700

    Modify if statement

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"gobasic/customer"
+)
 
 func main() {
-	// Defining variables to store values of type string, int, boo, and float64
-	var username string = "Go developer"
-	var age int = 12
-	var isAwesome = true
-	var height = 5.5
+	// Define the map
+	map1 := map[string]interface{}{
+		"name": "Some User",
+		"age":  35,
+		"address": map[string]interface{}{
+			"street": "Random St",
+			"city":   "Some town",
+			"state":  "Some state",
+			"zip":    123,
+		},
+	}
 
-	// Declaring pointers of types :string, int, bool and float64 using * operator
-	// Pointer to a string
-	var usernamePointer *string = &username
-	// Pointer to an integer
-	var agePointer *int = &age
-	// Pointer to a boolean
-	var isAwesomePointer *bool = &isAwesome
-	// Pointer to a floating number
-	var heightPointer *float64 = &height
+	// Convert the map to JSON
+	if jsonContent, err := json.MarshalIndent(map1, "", "    "); err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		// Print the JSON data
+		fmt.Println(string(jsonContent))
+	}
 
-	fmt.Printf("usernamePointer : %v ===> username value: %v \n", usernamePointer, *usernamePointer)
-	fmt.Printf("agePointer: %v  ===> age value: %v \n", agePointer, *agePointer)
-	fmt.Printf("isAwesomePointer: %v  ===> isAwesome value: %v \n", isAwesomePointer, *isAwesomePointer)
-	fmt.Printf("heightPointer : %v  ===> height value: %v \n", heightPointer, *heightPointer)
+	fmt.Println(customer.Name)
 }
